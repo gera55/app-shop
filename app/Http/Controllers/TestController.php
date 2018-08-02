ed33960cecdb248890147d3cebf8004e693f804c
gera55 <geramos55@gmail.com>
2018-08-02 14:28:21 -0300

    App advances

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Product;
 
 class TestController extends Controller
 {
-    public function welcome() {
-    	return view ('welcome');
+    public function welcome() 
+    {
+    	$products = Product::paginate(9);
+    	return view ('welcome')->with(compact('products')) ;
     } 
 }
